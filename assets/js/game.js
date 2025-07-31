925f39b17e3757192d25dad33b81ca8b4ba4b9a4
ljk28 <ljk28@hotmail.com>
2025-07-31 17:32:41 +0100

    Remove old game page logo and replace with new one

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@ document.getElementById("castle").onclick = function () {
     window.location.href = "index.html";
 };
 
-/* The above code was taken from and chnaged to fit this project https://www.geeksforgeeks.org/html/how-to-link-a-button-to-another-page-in-html/ */
+/* The above code was taken from and changed to fit this project https://www.geeksforgeeks.org/html/how-to-link-a-button-to-another-page-in-html/ */
 
 
  
