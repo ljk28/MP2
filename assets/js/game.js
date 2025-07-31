993f465970c81f2ef9d4b51a2ffc70612f6fabe0
ljk28 <ljk28@hotmail.com>
2025-07-31 15:05:15 +0100

    Add JS code to return to main/index page to reset game

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+document.getElementById("castle").onclick = function () {
+    window.location.href = "index.html";
+};
+
+/* The above code was taken from and chnaged to fit this project https://www.geeksforgeeks.org/html/how-to-link-a-button-to-another-page-in-html/ */
+
+
+ 
+
