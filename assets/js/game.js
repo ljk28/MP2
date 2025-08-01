6a1f18a68772fa07c9ac889a5f48a0ba79c9e776
ljk28 <ljk28@hotmail.com>
2025-08-01 16:50:55 +0100

    Create custom cursor with js

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,14 @@ document.getElementById("castle").onclick = function () {
 };
 
 /* The above code was taken from and changed to fit this project https://www.geeksforgeeks.org/html/how-to-link-a-button-to-another-page-in-html/ */
+/* the code below for the cursor was copied from ... https://dev.to/devshefali/3-steps-to-create-a-custom-cursor-with-html-css-and-javascript-2jo1 */
 
+const cursor = document.querySelector(".cursor");
 
+      document.addEventListener("mousemove", function (e) {
+        cursor.style.left = e.clientX + "px";
+
+        cursor.style.top = e.clientY + "px";
+      });
  
 
