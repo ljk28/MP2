fc5617a70a3f1e25546c3e83cb16a4ab7384e109
ljk28 <ljk28@hotmail.com>
2025-08-19 11:56:31 +0100

    Add more functions to JS page

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,5 @@
 
 /* --code taken and adapted from repo and tutorial https://github.com/WebDevSimplified/JavaScript-Text-Adventure/blob/master/game.js -- */
-
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
@@ -10,7 +9,33 @@ function startGame() {
   state= {}
   showTextNode(1)
 }
-
 function showTextNode(textNodeIndex) {
   const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+textNode.options.forEach(option => {
+  if (showOption(option)) {
+    const button = document.createElement('button')
+    button.innerText = option.text
+    button.classList.add('btn')
+    button.addEventListener('click', () => selectOption(option))
+    optionButtonsElement.appendChild(button)
+  }
+})
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
 }
