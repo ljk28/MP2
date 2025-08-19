3777f6cf6361cf43ed6edf89d8f909102a9bc858
ljk28 <ljk28@hotmail.com>
2025-08-19 10:24:50 +0100

    Add header tag and replace buttons with grid

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
-/* The code below was taken from and changed to fit this project https://www.geeksforgeeks.org/html/how-to-link-a-button-to-another-page-in-html/ */
-document.getElementById("castle").onclick = function () {
-    window.location.href = "index.html";
-};
-/* --- */
+
+/* ---- */
+
+const textElement = document.getElementById('text')
+const optionButtons
