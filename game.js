daaeb989a00d0511420f755a6e44d2d1427b57bf
ljk28 <ljk28@hotmail.com>
2025-08-20 19:01:59 +0100

    Add js onlick to html game page

--- game.js
+++ game.js
@@ -5,27 +5,14 @@ https://www.youtube.com/watch?v=Ffw_CFyieTs */
 let btnClick = document.querySelector("button");
 
 btnClick.addEventListener("click", () => {
-    window.location.href = "index.html";
-  })
-
-btnClick.addEventListener("click", () => {
     window.location.href = "game1.html";
   })
 
-  btnClick.addEventListenerBy("click",() => {
-    window.location.href = "game2.html";
-  })
 
- btnClick.addEventListenerBy("click",() => {
-    window.location.href = "game3.html";
-  })
-
-  btnClick.addEventListenerBy("click",() => {
-    window.location.href = "game4.html";
-  })
+ 
   /* Below the code is taken and adapted from ...https://www.shecodes.io/athena/3469-how-to-display-text-when-a-button-is-clicked-with-javascript */
   function displayText() {
-  var text = document.getElementById("");
+  var text = document.getElementById("text");
   text.style.display = "block";
 }
 
