eb3ec0490790183b862e2ece497e39a28ba90aaf
ljk28 <ljk28@hotmail.com>
2025-11-10 20:32:42 +0000

    add questions to  quiz

--- assets/quiz.js
+++ assets/quiz.js
@@ -17,10 +17,39 @@ const quizData = [
       answer: "cleaned up the cesspits and dirty corners of the castle"
     },
     {
-      question: "Who wrote 'Hamlet'?",
-      options: ["Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"],
+      question: "What did a barber cut in medieval Newcastle?",
+      options: ["Cut the corner", "Cut the cake", "hair and also, until it was banned by decree, would help with procedures like bleeding on unwitting patients", "barbed wire"],
       answer: "William Shakespeare"
     },
+    {
+      question: "'Is Blackfriars where they fry eggs and bacon?",
+      options: ["no", "it is a place where the priests and friars would live and work for the church", "yes, why not?", "I prefer grilled to fried food"],
+      answer: "it is a place where the priests and friars would live and work for the church"
+    },
+    {
+      question: "How old is the castle keep?",
+      options: ["too old!", "it was built last week", "around 100 years old", "finders keepers?"],
+      answer: "around 100 years old"
+    },
+    {
+        question: "where did Newcastle get its name from?",
+        options: ["A latin name Novum Castellum meaning new castle", "from a sand castle that got washed away", "some clever boffin who was bored", "from a man named Norman, nice chap"],
+        answer: "A latin name Novum Castellum meaning new castle"
+    },
+    {
+        question: "In medieval Newcastle, what was sold as a high commidity?",
+        options: ["wool, traded and sold a lot of it", "iphones", "pottery", "computer chips"],
+        answer: "River Tyne"
+    },
+    {
+        question: "Did Newcastle have a sheriff?",
+        options: ["No", "yes, they still do", "Get off your horse and drink and some milk", "wrong country, that's only in America"],
+        answer: "yes, they still do"
+    },
+
+
+    
+
     // Add more questions here...
   ];
   
@@ -70,5 +99,7 @@ const quizData = [
   
   showQuestion();
 
-
-    
+// End of code taken and adapted from:
+// https://medium.com/@codepicker57/building-an-interactive-quiz-with-html-css-and-javascript-efe9bd8129e2
+// Building an interactive quiz with HTML, CSS and JavaScript //
+// by Code Picker //
