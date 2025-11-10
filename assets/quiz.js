52acdec800825dc0af91a3b2676bfaeb9c6886e7
ljk28 <ljk28@hotmail.com>
2025-11-10 19:19:45 +0000

    add gitignire file

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,96 +1,14 @@
 /* jshint esversion: 6 */
 
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
+const startButton = document.getElementById('start');
 
 
-let shuffledQuestions, currentQuestionIndex;
-
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++;
-    setNextQuestion();
-});
 
 
-function startGame() {
 
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - '.5');
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    const questionElement = document.getElementById('question');
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button);
-    });
-}
-
-function resetState() {
-    clearStatusClass(document.body);
-    nextButton.classList.add('hide');
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
-
-function selectAnswer() {
-
-const selectButton = target;
-const correct = selectButton.dataset.correct;
-setStatusClass(document.body, correct);
-Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct);
-});
-}
+startButton.addEventListener('click', page2.html);
 
 
-nextButton.classList.remove('hide');
-if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide');
-} else {
-    startButton.innerText = 'Restart';
-    startButton.classList.remove('hide');
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-        element.classList.add('correct');
-    } else {
-        element.classList.add('wrong');
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
-}
-
-
-// submit button //
-function myFunction() {
-  var x = document.getElementById("mySubmit").name;
-  document.getElementById("demo").innerHTML = x;
-}
 
 const questions = [{
         question: 'Did Newcastle have knights?',
