ea2ed87fd3a2aed9f4f8ea3161128c683a58536e
ljk28 <ljk28@hotmail.com>
2025-11-14 15:37:12 +0000

    Fix punctuation in js quiz for questions in array

--- assets/quiz.js
+++ assets/quiz.js
@@ -7,44 +7,44 @@
 //
 const quizData = [
     {
-      question: 'Did Newcastle have knights?',
-      options: ["Maybe", "Thousands", "Yes", "No"],
+      question: "Did Newcastle have knights?",
+      options: ["Maybe", "Thousands", "Yes", "No, only nights and days"],
       answer: "Yes"
     },
     {
-      question: 'what did the Gongfarmers do in the castle?',
-      options: ["'Clang and clong'", "Is that even a real job? Come on!", "cleaned up the cesspits and dirty corners of the castle", "farmed for the now extinct animal, Gonglias"],
-      answer: "cleaned up the cesspits and dirty corners of the castle"
+      question: "What did the Gongfarmers do in the castle?",
+      options: ["Clang and clong", "Is that even a real job? Come on!", "Cleaned up the cesspits and dirty corners of the castle", "Farmed for the now extinct animal, Gonglias"],
+      answer: "Cleaned up the cesspits and dirty corners of the castle"
     },
     {
       question: "What did a barber cut in medieval Newcastle?",
-      options: ["Cut the corner", "Cut the cake", "hair and also, until it was banned by decree, would help with procedures like bleeding on unwitting patients", "barbed wire"],
-      answer: "hair and also, until it was banned by decree, would help with procedures like bleeding on unwitting patients"
+      options: ["Cut the corner", "Cut the cake", "Hair and also, until it was banned by decree, would help with procedures like bleeding on unwitting patients", "Barbed wire"],
+      answer: "Hair and also, until it was banned by decree, would help with procedures like bleeding on unwitting patients"
     },
     {
-      question: "'Is Blackfriars where they fry eggs and bacon?",
-      options: ["no", "it is a place where the priests and friars would live and work for the church", "yes, why not?", "I prefer grilled to fried food"],
-      answer: "it is a place where the priests and friars would live and work for the church"
+      question: "Is Blackfriars where they fry eggs and bacon?",
+      options: ["No", "It is a place where the priests and friars would live and work for the church", "yes, I've seen their frying pan collection", "I prefer grilled to fried food"],
+      answer: "It is a place where the priests and friars would live and work for the church"
     },
     {
       question: "How old is the castle keep?",
-      options: ["too old!", "it was built last week", "around 100 years old", "finders keepers?"],
-      answer: "around 100 years old"
+      options: ["Too old!", "It was built last week", "Around 100 years old", "Finders keepers?"],
+      answer: "Around 100 years old"
     },
     {
-        question: "where did Newcastle get its name from?",
-        options: ["A latin name Novum Castellum meaning new castle", "from a sand castle that got washed away", "some clever boffin who was bored", "from a man named Norman, nice chap"],
+        question: "Where did Newcastle get its name from?",
+        options: ["A latin name Novum Castellum meaning new castle", "From a sand castle that got washed away", "Some clever boffin who was bored", "From a man named Norman, nice chap"],
         answer: "A latin name Novum Castellum meaning new castle"
     },
     {
         question: "In medieval Newcastle, what was sold as a high commidity?",
-        options: ["wool, traded and sold a lot of it", "iphones", "pottery", "computer chips"],
-        answer: "wool, traded and sold a lot of it"
+        options: ["Wool, traded and sold a lot of it", "Iphones", "Pottery", "Computer Chips"],
+        answer: "Wool, traded and sold a lot of it"
     },
     {
         question: "Did Newcastle have a sheriff?",
-        options: ["No", "yes, they still do", "Get off your horse and drink and some milk", "wrong country, that's only in America"],
-        answer: "yes, they still do"
+        options: ["No", "Yes, they still do", "Get off your horse and drink and some milk", "Wrong country, that's only in America"],
+        answer: "Yes, they still do"
     },
     
 
